99400e469c56d2bbaecd387f52e2667031ae900b
Beatriz Narciso <147112758+beatriznarf@users.noreply.github.com>
2024-06-12 19:35:34 -0300

    Adicionando o recurso "Paciente"
    Adicionando o recurso "Paciente"  e os pacotes "models" e "dtos" na clinica agendamento

--- clinica-agendamento/src/main/java/br/edu/imepac/ClinicaAgendamento.java
+++ clinica-agendamento/src/main/java/br/edu/imepac/ClinicaAgendamento.java
@@ -1,4 +1,21 @@
 package br.edu.imepac;
 
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
 public class ClinicaAgendamento {
+    public static void main (String[] args) {
+        SpringApplication.run(ClinicaAgendamento.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+
 }
---
+++ clinica-agendamento/src/main/java/br/edu/imepac/dtos/PacientesDtos.java
@@ -0,0 +1,27 @@
+package br.edu.imepac.dtos;
+
+import lombok.Data;
+
+@Data
+public class PacientesDtos {
+
+    private Long id;
+    private String nome;
+    private int rg;
+    private String orgaoEmissor;
+    private int cpf;
+    private String endereco;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String telefone;
+    private String celular;
+    private String sexo;
+    private String nascimento;
+    private String possuiConvenio;
+    private String nomeConvenio;
+
+
+}
